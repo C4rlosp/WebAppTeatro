ec7bfe020ec64ea93d6230dbc9569fc026dc30d7
C4rlosp <carlos.perezp@uhispano.ac.cr>
2024-06-13 17:07:29 -0600

    fixed 10

--- WebAppTeatro.Shared/Usuarios.cs
+++ WebAppTeatro.Shared/Usuarios.cs
@@ -8,3 +8,12 @@ namespace WebAppTeatro.Shared
 {
     public class Usuarios
     {
+        public int IdUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public int Cedula {  get; set; }
+        public int Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Password { get; set; }
+    }
+}
