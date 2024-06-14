7ecca75c999065503323800e9fc78ea6119b4327
C4rlosp <carlos.perezp@uhispano.ac.cr>
2024-06-14 17:55:43 -0600

    fixed

---
+++ WebAppTeatro.Shared/FuncionesTeatrales.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAppTeatro.Shared
+{
+    public class FuncionesTeatrales
+    {
+        public int IdFuncion { get; set; }
+        public string Titulo { get; set; }
+        public double Precio { get; set; }
+        public string Descripcion { get; set; }
+        public string Categoria { get; set; }
+        public string ImagenUrl { get; set; }
+        public string Horario { get; set; }
+    }
+
+}
